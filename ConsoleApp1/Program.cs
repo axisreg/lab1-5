ddfb929b07ccabd6ad356fb7cdd00aa67a6d137b
Anna Butkova <axisreg@gmail.com>
2017-11-11 21:04:27 +0300

    Оформляем консольное приложение

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,15 +10,53 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            List<LibraryCard> cards = new List<LibraryCard>()
+            System.Console.WriteLine("Задание библиотечной карточки:");
+            System.Console.WriteLine("Введите цифру для создания карточки определенного типа:");
+            System.Console.WriteLine("1) Книга");
+            System.Console.WriteLine("2) Журнал");
+            try
             {
-                new Magazine("Прогнило чёто в", 1800, 20, 4),
-                new Book("В датском королевстве", 1992, 400, "Е. Розцекранц, И. Гильденстерн", "Дача inc")
-            };
-
-            foreach (LibraryCard pub in cards) {
-                System.Console.WriteLine(pub);
+                int cardType = Convert.ToInt32(System.Console.ReadLine());
+                LibraryCard card;
+                switch (cardType)
+                {
+                    case 1:
+                        System.Console.WriteLine("Создается карточка книги");
+                        card = new Book();
+                        System.Console.WriteLine("Введите заглавие книги:");
+                        card.Title = Console.ReadLine();
+                        System.Console.WriteLine("Введите год издания книги:");
+                        card.Year = Convert.ToInt32(Console.ReadLine());
+                        System.Console.WriteLine("Введите количество страниц книги:");
+                        card.Pages = Convert.ToInt32(Console.ReadLine());
+                        System.Console.WriteLine("Введите список авторов книги (через запятую):");
+                        ((Book)card).Authors = Console.ReadLine();
+                        System.Console.WriteLine("Введите издательство книги:");
+                        ((Book)card).Publisher = Console.ReadLine();
+                        break;
+                    case 2:
+                        System.Console.WriteLine("Создается карточка журнала");
+                        card = new Magazine();
+                        System.Console.WriteLine("Введите заглавие журнала:");
+                        card.Title = Console.ReadLine();
+                        System.Console.WriteLine("Введите год издания журнала:");
+                        card.Year = Convert.ToInt32(Console.ReadLine());
+                        System.Console.WriteLine("Введите количество страниц журнала:");
+                        card.Pages = Convert.ToInt32(Console.ReadLine());
+                        System.Console.WriteLine("Введите номер журнала:");
+                        ((Magazine)card).Number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    default:
+                        throw new ArgumentException("Неизвестный тип библиотечной карточки");
+               }
+                Console.WriteLine("Форматирование карточки по ГОСТ:");
+                Console.WriteLine(card.Format());
             }
+            catch (Exception ex) {
+                System.Console.WriteLine("Во время выполнения программы возникла ошибка: " + ex.Message);
+            }
+
+            System.Console.WriteLine("Нажмите 'Ввод' для выхода.");
             System.Console.ReadLine();
 
         }
